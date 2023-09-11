4301071b09d60b221c97bd7cc491e30953bdca24
ragul0427 <ragulhp2704@gmail.com>
2023-09-11 14:32:00 +0530

    erheewe

--- libs/conn.js
+++ libs/conn.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   // mongodb://127.0.0.1:27017/Crud13
   // mongodb+srv://ragul8523947145:ragulhp2704@crud.q6zjhg8.mongodb.net/Crud?retryWrites=true&w=majority
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Crud13");
+    await mongoose.connect("mongodb+srv://ragul8523947145:ragulhp2704@crud.q6zjhg8.mongodb.net/Crud?retryWrites=true&w=majority");
   } catch (err) {
     console.log(err,"werhuierwh");
   }
